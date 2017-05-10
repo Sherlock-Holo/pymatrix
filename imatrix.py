46a126ce2c8623aed063a0251187a9fdc594a2df
VampireSH <sherlockya@gmail.com>
2017-05-10 19:37:17 +0800

    mission complete

---
+++ imatrix.py
@@ -0,0 +1,28 @@
+import numpy
+import sys
+
+def gen_matrix():
+    matrix = []
+
+    line = int(input('Please tell me how many lines in the matrix: '))
+
+    for n in range(line):
+        l = input('The {} line: '.format(n + 1))
+        matrix_line = l.split()
+        matrix_line = list(map(int, matrix_line))
+        matrix.append(matrix_line)
+
+    return matrix
+
+matrix = gen_matrix()
+matrix = numpy.mat(matrix)
+
+if int(numpy.linalg.det(matrix)) == 0:
+    print("The matrix can't calculate the inv matrix")
+    sys.exit(1)
+
+else:
+    imatrix = matrix.I
+
+    for n in imatrix:
+        print(n)
