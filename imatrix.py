c665f7b80d04c4c3d5184dc83cd05cb793ac46b0
VampireSH <sherlockya@gmail.com>
2017-05-31 01:42:15 +0800

    mix program source...

--- imatrix.py
+++ imatrix.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 
-import numpy
 import sys
+'''this is for get your content from terminal'''
+'''use sys.argv better than argparse'''
+
 
 def gen_matrix():
+    '''this is turn you input content to a matrix'''
     matrix = []
 
     line = int(input('Please tell me how many lines in the matrix: '))
@@ -16,10 +19,15 @@ def gen_matrix():
 
     return matrix
 
-matrix = gen_matrix()
-matrix = numpy.mat(matrix)
 
-if int(numpy.linalg.det(matrix)) == 0:
+'''use gen_matrix to return a matrix'''
+from numpy import mat as num_mat
+'''i think i can use lambda function to make the program simpler'''
+from numpy import linalg as num_linalg
+matrix = gen_matrix()
+matrix = num_mat(matrix)
+
+if int(num_linalg.det(matrix)) == 0:
     print("The matrix can't calculate the inv matrix")
     sys.exit(1)
 
